9e65ee9057d7902dd626b6ba299c3d32979b7b24
nison79 <georgenison@gmail.com>
2020-09-25 16:23:03 +0300

    day 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 import './App.css';
+import { Button, TextField } from "@material-ui/core"
+import Todo from "./Todo"
+
 
 function App() {
   const [inputText,setInputText] = useState("");
   const [todos,setTodos] = useState([]);
 
-  const addATodo = () => {
-    
-    setTodos([...todos,inputText]);
+  const addATodo = (event) => {
+    event.preventDefault();
+    setTodos([inputText,...todos]);
     setInputText("");
     console.log("todos list", [...todos,inputText]);
   }
@@ -15,13 +18,35 @@ function App() {
 
   return (
     <div className="app">
-      <h1>Todo app</h1>
+      <h1>Plan your Day</h1>
+      <div className = "app__textButton">
+        <form>
+          <TextField
+          placeholder = "Write here"
+          value={inputText} 
+          onChange={event => setInputText(event.target.value)} 
+          type = "text">
+          </TextField>
 
-      <input 
-      value={inputText} onChange={event => setInputText(event.target.value)} 
-      type = "text">
-      </input>
-      <button onClick ={addATodo} >Add a TODO</button>
+          <Button
+          size= "large"
+          disabled ={!inputText}
+          type = "submit"
+          style ={{marginLeft : "40px" , background: "pink",color:"black"}}
+          color = "secondary"
+          variant = "outlined" 
+          onClick ={addATodo} >
+          Add a Plan
+          </Button>
+
+        </form>
+      </div>
+
+    {todos.map((todo) => (
+        <Todo text ={todo}/>
+    ))}
+
+
     </div>
   );
 }
